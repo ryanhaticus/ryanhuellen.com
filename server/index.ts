a82e2174335b1b25ff716f83e87512607ad93e68
Ryan Huellen <github@ryanhuellen.com>
2024-10-05 21:59:44 -0500

    fix: using esm dirname

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import path from 'path';
 const app = express();
 
 app.use(express.static(
-  path.resolve(__dirname, '..', 'dist')
+  path.resolve(import.meta.dirname, '..', 'dist')
 ));
 
 const PORT = 3000 as const;
