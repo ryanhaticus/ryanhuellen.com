ed1a63b16da6e988d5810d63efb201d5f20bf065
Ryan Huellen <github@ryanhuellen.com>
2024-10-05 23:42:46 -0500

    feat: the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="min-h-screen flex items-center p-8 justify-center text-white bg-zinc-950 bg-[length:40px_40px] bg-grid">
+			<div>
+				<header>
+					<h1 className="text-6xl font-bold">ryan huellen</h1>
+				</header>
+				<main className="mt-8 text-2xl font-semibold">
+					<p>
+						I'm a software engineer enabling payments at John Deere Financial.
+					</p>
+					<p className="mt-2 md:mt-0">
+						I also maintain developer tooling like{" "}
+						<a
+							href="https://github.com/ryanhaticus/pretzelduck"
+							target="_blank"
+							rel="noreferrer"
+							className="text-red-600"
+						>
+							Pretzel Duck
+						</a>{" "}
+						and{" "}
+						<a
+							href="https://draw.ryanhuellen.com"
+							target="_blank"
+							rel="noreferrer"
+							className="text-orange-600"
+						>
+							draw.ryanhuellen.com
+						</a>
+						.
+					</p>
+					<div className="mt-8 flex flex-col md:flex-row gap-6 text-center md:text-inherit">
+						<a
+							href="/resume.pdf"
+							download="ryan-huellen-resume.pdf"
+							className="border-8 px-10 py-2 rounded-xl hover:border-yellow-600"
+						>
+							résumé
+						</a>
+						<a
+							href="https://github.com/ryanhaticus"
+							target="_blank"
+							rel="noreferrer"
+							className="border-8 px-10 py-2 rounded-xl hover:border-green-600"
+						>
+							GitHub
+						</a>
+						<a
+							href="https://www.linkedin.com/in/ryan-huellen"
+							target="_blank"
+							rel="noreferrer"
+							className="border-8 px-10 py-2 rounded-xl hover:border-blue-600"
+						>
+							LinkedIn
+						</a>
+					</div>
+				</main>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'grid': 'radial-gradient(circle, #18181b 1px, rgba(0, 0, 0, 0) 1px)'
+      }
+    },
+  },
+  plugins: [],
+}
+
